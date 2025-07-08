c28087245514b1a8eefb7cef8108db491185d42c
Kaso <phamcam102016@gmail.com>
2025-07-08 20:52:44 +0700

    feat: add CORS middleware for frontend integration

--- app/main.py
+++ app/main.py
@@ -1,9 +1,20 @@
 """Main application"""
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.user_router import router as authentication_router
 
 app = FastAPI()
 
+# Middlewares
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routers
 app.include_router(authentication_router)
