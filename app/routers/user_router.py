f26596ef5784d42cb18ca79e5da9946bef477caf
Kaso <phamcam102016@gmail.com>
2025-07-28 20:40:26 +0700

    refactor(auth): fix dependency injection

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,6 +1,6 @@
 """Module providing functions for authentication endpoint"""
 
-from fastapi import APIRouter, status, BackgroundTasks, Query
+from fastapi import APIRouter, status, BackgroundTasks, Query, Depends, Response
 
 from app.schemas.user_schema import (
     UserLoginRequest,
@@ -11,8 +11,19 @@ from app.schemas.user_schema import (
     ResetPasswordRequest,
 )
 from app.services.user_service import UserService
+from app.repositories.user_repository import UserRepository
 
 router = APIRouter(prefix="/api/auth", tags=["Users"])
+
+
+def get_user_repository() -> UserRepository:
+    return UserRepository()
+
+
+def get_user_service(
+    user_repo: UserRepository = Depends(get_user_repository),
+) -> UserService:
+    return UserService(user_repo)
 
 
 @router.post(
@@ -22,17 +33,12 @@ router = APIRouter(prefix="/api/auth", tags=["Users"])
     status_code=status.HTTP_200_OK,
     response_model_by_alias=False,
 )
-async def login(request: UserLoginRequest):
-    """Login endpoint
-
-    Args:
-        request (UserLoginRequest): Email and password
-
-    Returns:
-        UserLoginResponse: Access token
-    """
-    service = UserService()
-    return await service.login_user(request)
+async def login(
+    request: UserLoginRequest,
+    response: Response,
+    user_service: UserService = Depends(get_user_service),
+):
+    return await user_service.login_user(request=request, response=response)
 
 
 @router.post(
@@ -42,17 +48,10 @@ async def login(request: UserLoginRequest):
     status_code=status.HTTP_201_CREATED,
     response_model_by_alias=False,
 )
-async def register(request: UserRegisterRequest):
-    """Register endpoint
-
-    Args:
-        request (UserRegisterRequest): Email, username and password
-
-    Returns:
-        UserRegisterResponse: Message and user Id
-    """
-    service = UserService()
-    return await service.register_user(request)
+async def register(
+    request: UserRegisterRequest, user_service: UserService = Depends(get_user_service)
+):
+    return await user_service.register_user(request)
 
 
 @router.post(
@@ -61,9 +60,26 @@ async def register(request: UserRegisterRequest):
     status_code=status.HTTP_200_OK,
     response_model_by_alias=False,
 )
-async def forgot_password(request: ForgotPasswordRequest, bg: BackgroundTasks):
-    service = UserService()
-    return await service.forgot_password(request, bg)
+async def forgot_password(
+    request: ForgotPasswordRequest,
+    bg: BackgroundTasks,
+    user_service: UserService = Depends(get_user_service),
+):
+    """
+    Forgot password route to send forgot password email
+
+    Args:
+        request (ForgotPasswordRequest): Request schema
+        bg (BackgroundTasks): Handle background tasks
+        user_service (UserService, optional): Inject UserService class
+
+    Raises:
+        HTTPException: 500 Internal Server Error
+
+    Returns:
+        JSON: {"msg": "Email sent"}
+    """
+    return await user_service.forgot_password(request, bg)
 
 
 @router.post(
@@ -72,6 +88,43 @@ async def forgot_password(request: ForgotPasswordRequest, bg: BackgroundTasks):
     status_code=status.HTTP_200_OK,
     response_model_by_alias=False,
 )
-async def reset_password(data: ResetPasswordRequest, token: str = Query(...)):
-    service = UserService()
-    return await service.reset_password(data, token)
+async def reset_password(
+    data: ResetPasswordRequest,
+    token: str = Query(...),
+    user_service: UserService = Depends(get_user_service),
+):
+    """
+    Reset password route
+
+    Args:
+        data (ResetPasswordRequest): Reset password schema
+        token (str, optional): Forgot password JWT token. Defaults to Query(...).
+        user_service (UserService, optional): Inject UserService class
+
+    Raises:
+        HTTPException: 500 Internal Server Error
+
+    Returns:
+        JSON: {"msg": "Password updated successfully"}
+    """
+    return await user_service.reset_password(data, token)
+
+
+@router.post("/logout")
+async def logout(
+    response: Response, user_service: UserService = Depends(get_user_service)
+):
+    """
+    Logout user route
+
+    Args:
+        response (Response): Used to store JWT token in to HTTP cookie
+        user_service (UserService, optional): Inject UserService class
+
+    Raises:
+        HTTPException: 500 Internal Server Error
+
+    Returns:
+        JSON: {"message": "Successfully logged out"}
+    """
+    return await user_service.logout(response)
