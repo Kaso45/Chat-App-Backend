e14b7475a2d7b5e2cfd006f0e4a121eaa214b916
Kaso <phamcam102016@gmail.com>
2025-07-30 13:48:23 +0700

    fix: remove unused importation

--- app/main.py
+++ app/main.py
@@ -5,8 +5,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers.user_router import router as authentication_router
-from app.websocket.websocket import router as websocket_router
-from app.routers.chat_router import router as chat_router
 from app.repositories.user_repository import UserRepository
 
 
@@ -33,5 +31,3 @@ app.add_middleware(
 
 # Routers
 app.include_router(authentication_router)
-app.include_router(chat_router)
-app.include_router(websocket_router)
