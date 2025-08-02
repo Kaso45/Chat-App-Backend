624a8657438a005083963e324db47e4a1b40d561
Kaso45 <phamcam102016@gmail.com>
2025-08-02 16:41:40 +0700

    refactor: fix method and add type check

--- app/websocket/websocket.py
+++ app/websocket/websocket.py
@@ -3,8 +3,8 @@ from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
 
 from app.dependencies import get_current_user_ws
 from app.schemas.websocket_schema import WebsocketReceivePayload
+from app.services.message_service import MessageService
 from app.websocket.websocket_manager import manager
-from app.services.chat_service import ChatService
 from app.repositories.chat_repository import ChatRepository
 from app.repositories.message_repository import MessageRepository
 
@@ -21,18 +21,18 @@ def get_message_repository() -> MessageRepository:
     return MessageRepository()
 
 
-def get_chat_service(
+def get_message_service(
     chat_repo: ChatRepository = Depends(get_chat_repository),
     message_repo: MessageRepository = Depends(get_message_repository),
-) -> ChatService:
-    return ChatService(chat_repo, message_repo)
+):
+    return MessageService(chat_repo, message_repo)
 
 
 @router.websocket("")
 async def websocket_endpoint(
     websocket: WebSocket,
     user_id: str = Depends(get_current_user_ws),
-    chat_service: ChatService = Depends(get_chat_service),
+    message_service: MessageService = Depends(get_message_service),
 ):
     await manager.connect(websocket, user_id)
     try:
@@ -44,12 +44,17 @@ async def websocket_endpoint(
 
             if event_type == "load_chat":
                 chat_id = payload_obj.chat_id
-                history = await chat_service.get_cache_messages(chat_id)
+                history = await message_service.get_cache_messages(chat_id)
                 await websocket.send_json(history)
             elif event_type == "new_message":
                 message_data = payload_obj.data
                 chat_id = payload_obj.chat_id
-                await chat_service.handle_new_message(
+
+                if message_data is None:
+                    await websocket.send_json({"error": "Missing message data"})
+                    return
+
+                await message_service.handle_new_message(
                     message=message_data, chat_id=chat_id, sender_id=user_id
                 )
     except WebSocketDisconnect:
