b128c2cf931a596a1c23d3e69ce4ec99582d5e5f
Kaso45 <phamcam102016@gmail.com>
2025-08-02 16:34:00 +0700

    fix: fix redis importation

--- app/redis_client.py
+++ app/redis_client.py
@@ -1,10 +1,10 @@
 """Redis config"""
 
-import redis.asyncio as redis
+from redis.asyncio import Redis
 
 from app.config.config import settings
 
-r = redis.Redis(
+r = Redis(
     host="redis-17220.c295.ap-southeast-1-1.ec2.redns.redis-cloud.com",
     port=17220,
     decode_responses=True,
@@ -18,3 +18,7 @@ def redis_chat_key(chat_id: str) -> str:
     Redis key for chat room
     """
     return f"chat_id:{chat_id}:messages"
+
+
+def redis_user_chat_key(user_id: str) -> str:
+    return f"chat_list:user_id:{user_id}"
